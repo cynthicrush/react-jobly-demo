a0f0a4fee90c8c4c977444a9cbf088d32335ddca
cynthicrush <cynthiazcanada@gmail.com>
2022-07-15 18:45:18 -0500

    new files added

---
+++ frontend/build/precache-manifest.954aef3ccdd3f2c9603f946383237d6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff7d7943a67df28e316",
+    "url": "/static/css/main.c6e21d39.chunk.css"
+  },
+  {
+    "revision": "aff7d7943a67df28e316",
+    "url": "/static/js/main.aff7d794.chunk.js"
+  },
+  {
+    "revision": "62a3782746b8bc79922d",
+    "url": "/static/css/1.0c72a6f1.chunk.css"
+  },
+  {
+    "revision": "62a3782746b8bc79922d",
+    "url": "/static/js/1.62a37827.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a2b932f9332fdf51231221b8a1a9a875",
+    "url": "/index.html"
+  }
+];
